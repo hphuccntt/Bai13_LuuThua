fc3feae2362529de6d94e811ba899be2ea27fca7
hphuccntt <phuc54212420014@bachkhoasaigon.edu.vn>
2024-11-18 11:11:38 +0700

    Add project files.

---
+++ Bai13_LuuThua.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "tinh luy thua" << endl;
+	int so, mu, luythua=1;
+	cout << "nhap so: "; cin >> so;
+	cout << "nhap mu: "; cin >> mu;
+	for (int i = 0; i < mu; i++)
+	{
+		luythua *= so;
+	}
+	cout << so << "*" << mu << "=" << luythua;
+	return 0;
+}
